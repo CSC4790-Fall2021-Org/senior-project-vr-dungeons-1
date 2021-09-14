27233a2bcdb4dfd7e918c81819ea4b35ee4dfa8a
kfsofronas <kfsofronas@gmail.com>
2021-09-14 17:36:47 -0400

    Move files

---
+++ GeneratorCode/TabletopDungeonGenerator/CellularAutomataRandomizer.java
@@ -0,0 +1,25 @@
+package main;
+import java.util.Arrays;
+
+public class CellularAutomataRandomizer {
+		
+		//use this template file to make your dungeon layout randomizer
+		//be sure to try to implement the seed so the randomizer consistently outputs the same thing using any given seed
+		public static boolean[][] randomize(boolean[][] in, int seed) {
+			//d is the temporary array that you'll use to make the layout, currently initialized as all False values.
+			boolean[][] d = new boolean[in.length][in[0].length];
+	        helper help = new helper();
+	       	d = help.generateMap(in.length,in[0].length);
+			return d;
+		}
+		
+		public static void main(String args[]) {
+			//this is the test method, it prints out the random dungeon with a seed of 1234 at the default size
+			Dungeon dun = new Dungeon(1234);
+			dun.setLayout(randomize(dun.d, dun.SEED));
+//			System.out.println(Arrays.toString(dun.d));
+			DungeonViewer dv = new DungeonViewer(dun,10);
+			dv.setVisible(true);
+		}
+		
+}
