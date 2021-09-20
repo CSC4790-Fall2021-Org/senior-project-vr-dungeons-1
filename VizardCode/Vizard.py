cdbfd8504eeff0c10c59c2dfab7ef88ddddbf49b
kfsofronas <kfsofronas@users.noreply.github.com>
2021-09-20 17:50:07 -0400

    Start messing around with vizshape, added a grid and the first floor panel

--- VizardCode/Vizard.py
+++ VizardCode/Vizard.py
@@ -1,4 +1,11 @@
 ﻿import viz
 import vizact
+import vizshape
 
+viz.fov(90)
 viz.go()
+
+vizshape.addGrid(color=[0.2]*3).setPosition([0.5,1,0.5])
+
+floor = vizshape.addQuad(size=(1.0,1.0),axis=vizshape.AXIS_Y,texture=viz.add("white.jpg"),)
+floor.setPosition([1,1,0])
