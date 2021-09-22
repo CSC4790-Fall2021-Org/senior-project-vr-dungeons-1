0927713b34251cf16339fa7ce9f0aa6880111333
Christina LaRow <clarow@villanova.edu>
2021-09-22 13:04:53 -0400

    added white walls

--- VizardCode/Vizard.py
+++ VizardCode/Vizard.py
@@ -12,8 +12,8 @@ viz.go()
 floor = vizshape.addQuad(size=(1.0,1.0),axis=vizshape.AXIS_Y,texture=viz.add("white.jpg"),lighting=False)
 floor.setPosition([1,-1,0])
 
-#reads from the csv file in GeneratorCode
-with open('../GeneratorCode/output.csv') as csv_file:
+#reads from the csv file in GeneratorCode rechange to open('../GeneatorCode/output.csv)
+with open('C:/Users/Christina LaRow/CSC-4790/senior-project-vr-dungeons-1/GeneratorCode/output.csv') as csv_file:
 	reader = csv.reader(csv_file, delimiter=',')
 	data = list(reader)[0]
 
@@ -36,7 +36,8 @@ for i in range(0,len(data)):
 #row and col temp variables for counting, starting at 0 with the first row+=1 and col+=1
 row = -1
 col = -1
-
+box = vizshape.addBox(size=(1.0,1.0,1.0))
+box.color(viz.WHITE)
 #iterate over every entry in the 2d list
 for r in layout:
 	row+=1
@@ -46,5 +47,10 @@ for r in layout:
 		#if there should be a floor at (row,col), clone the master floor to (row,1,col)
 		if(entry=="false"):
 			floor.copy().setPosition(row,1,col)
+		else:
+			box.copy().setPosition(row,1,col)
+			box.copy().setPosition(row,2,col)
+			box.copy().setPosition(row,3,col)
+			
 			
 print("Done")
