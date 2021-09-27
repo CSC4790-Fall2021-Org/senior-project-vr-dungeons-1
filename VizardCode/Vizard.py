0e3ffd7e123f7c652d544a1eec54007a321e8f9a
Connor618 <cskeving@villanova.edu>
2021-09-27 13:00:50 -0400

    Added torchlight functionality

--- VizardCode/Vizard.py
+++ VizardCode/Vizard.py
@@ -25,8 +25,16 @@ viz.go()
 
 #vizshape.addGrid(color=[0.2]*3).setPosition([0.5,1,0.5])
 
+#Changes how lighting works around the main view
+myLight = viz.MainView.getHeadLight()
+myLight.disable()
+myLight.color(viz.ORANGE)
+myLight.intensity(0.5)
+vizact.onkeydown('f', myLight.enable)
+vizact.onkeydown('g', myLight.disable)
+
 #creates the master floor tile from which every other tile will be cloned and sets it at position [1,-1,0], underneath the floor
-floor = vizshape.addQuad(size=(1.0,1.0),axis=vizshape.AXIS_Y,texture=viz.add("white.jpg"),lighting=False)
+floor = vizshape.addQuad(size=(1.0,1.0),axis=vizshape.AXIS_Y,texture=viz.add("white.jpg"),lighting=True)
 floor.setPosition([1,-1,0])
 
 #reads from the csv file in GeneratorCode rechange to open('../GeneatorCode/output.csv)
