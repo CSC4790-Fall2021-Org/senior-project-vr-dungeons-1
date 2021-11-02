a316db6dfb3e71bfffea8295ba604f8404723aa5
kfsofronas <kfsofronas@users.noreply.github.com>
2021-11-02 15:25:52 -0400

    Fix runtime bug regarding non-square dungeons

--- GeneratorCode/TabletopDungeonGenerator/Voronoi.java
+++ GeneratorCode/TabletopDungeonGenerator/Voronoi.java
@@ -47,7 +47,7 @@ public class Voronoi extends Dungeon {
 	
 	public boolean[][] randomize(int cell) {
 		//d is the temporary array that you'll use to make the layout, currently initialized as all False values.
-		boolean[][] dun = new boolean[d.length][d[0].length];
+		boolean[][] dun = new boolean[X][Y];
 		
 		//randomize the dungeon here
 		//chooses random points to be nodes
@@ -56,22 +56,22 @@ public class Voronoi extends Dungeon {
 		int[] px = new int[cell];
 		int[] py = new int[cell];
 		for (int i = 0; i < cell; i++) {
-			px[i] = rand.nextInt(d[0].length);
-			py[i] = rand.nextInt(d.length);
+			px[i] = rand.nextInt(X);
+			py[i] = rand.nextInt(Y);
  
 		}
 		
 		//for each point x,y in the dungeon, it finds the nearest node. If the node number is n%3==2, it becomes open space, if not it becomes a wall.
-		for (int x = 0; x < d[0].length; x++) {
-			for (int y = 0; y < d.length; y++) {
+		for (int c = 0; c < X; c++) {
+			for (int r = 0; r < Y; r++) {
 				n = 0;
 				for (int i = 0; i < cell; i++) {
-					if (distance(px[i], x, py[i], y) < distance(px[n], x, py[n], y)) {
+					if (distance(px[i], c, py[i], r) < distance(px[n], c, py[n], r)) {
 						n = i; //find the closest point to x,y
 					}
 				}
 				if(n%3==0||n%3==1) {
-					dun[x][y]= true;
+					dun[c][r]= true;
 				}
 			}
 		}
